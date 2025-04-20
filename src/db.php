ce6d291c8e0b406e03ce3735bdda098a43375a51
bettybemanar <bettybemanar@gmail.com>
2025-04-20 11:23:02 +0200

    the database edit

--- src/db.php
+++ src/db.php
@@ -1,11 +1,12 @@
 <?php
-$host = "mysql";  
-$dbname = "myapp";
-$username = "root";
-$password = "rootpassword";
+$host = getenv("MYSQLHOST") ?: "mysql.railway.internal";
+$port = getenv("MYSQLPORT") ?: "3306";
+$dbname = getenv("MYSQLDATABASE") ?: "railway";
+$username = getenv("MYSQLUSER") ?: "root";
+$password = getenv("MYSQLPASSWORD") ?: "GKSZjLITJlmBZQveVgoHpSrBMICaUQTX";
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("فشل الاتصال بقاعدة البيانات: " . $e->getMessage());
