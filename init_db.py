ef6b5d9441b6d229d27eca7083498a3fa3bc1b7f
bettybemanar <bettybemanar@gmail.com>
2025-04-20 12:48:31 +0200

    last edit

--- init_db.py
+++ init_db.py
@@ -21,6 +21,10 @@ CREATE TABLE IF NOT EXISTS users (
 )
 """)
 
+cursor.execute("""
+INSERT IGNORE INTO users (username, password)
+VALUES (%s, %s)
+""", ("admin", "passtest"))
 
 
 print("✅ تم إنشاء قاعدة البيانات والجداول")
